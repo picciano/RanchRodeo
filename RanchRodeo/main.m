319756690e3879e8fb65cce226435add707b189d
Anthony Picciano <anthony.picciano@gmail.com>
2014-05-22 15:13:42 -0600

    Initial Commit

---
+++ RanchRodeo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RanchRodeo
+//
+//  Created by Anthony Picciano on 5/22/14.
+//  Copyright (c) 2014 Anthony Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RRAppDelegate class]));
+    }
+}
